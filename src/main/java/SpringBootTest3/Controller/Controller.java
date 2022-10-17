2dee88becb8d7897bb686d9f16d235546b9a49e6
satvik3110 <satvik3110@gmail.com>
2022-10-17 16:07:01 +0530

    Added sql queries in application.properties

--- src/main/java/SpringBootTest3/Controller/Controller.java
+++ src/main/java/SpringBootTest3/Controller/Controller.java
@@ -1,13 +1,16 @@
 package SpringBootTest3.Controller;
 
 import SpringBootTest3.Domain.Country;
+import SpringBootTest3.Domain.Language;
 import SpringBootTest3.Preservance.CountryRepository;
+import SpringBootTest3.Preservance.LanguageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/countries")
